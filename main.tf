9eec970bf6f7f7e14e2a5242d0e79804c73b9a50
Pratik Anil Yadav <91407633+propratik31@users.noreply.github.com>
2024-10-28 22:10:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "test-server" {
-  ami = "ami-06b21ccaeff8cd686"
+  ami = "ami-0866a3c8686eaeeba"
   instance_type = "t2.micro"
   key_name = "myjenkins"
-  vpc_security_group_ids = ["sg-04cf4e3801bd09203"]
+  vpc_security_group_ids = ["sg-05b5859d9fb7b7c98"]
   connection {
      type = "ssh"
      user = "ubuntu"
