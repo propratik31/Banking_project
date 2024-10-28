be606d463075a465dfea8f2206862353387e1c4a
Pratik Anil Yadav <91407633+propratik31@users.noreply.github.com>
2024-10-28 21:57:12 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,5 @@
 provider "aws" {
-region = "us-east-1"
+region                  = "us-east-1"
+access_key              = "AKIAZDZTBY6WSBDQZ6XX"
+secret_key              = "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"
 }
