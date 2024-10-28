5fa8464782be8c60a01c33007ce9b3f2f94bcaab
Pratik Anil Yadav <91407633+propratik31@users.noreply.github.com>
2024-10-29 00:05:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,29 @@
 resource "aws_instance" "test-server" {
-  ami = "ami-0866a3c8686eaeeba"
-  instance_type = "t2.micro"
-  key_name = "myjenkins"
+  ami               = "ami-0866a3c8686eaeeba"
+  instance_type     = "t2.micro"
+  key_name          = "myjenkins"
   vpc_security_group_ids = ["sg-05b5859d9fb7b7c98"]
+
   connection {
-     type = "ssh"
-     user = "ubuntu"
-     private_key = file("./myjenkins.pem")
-     host = self.public_ip
-     }
+    type        = "ssh"
+    user        = "ubuntu"     
+    private_key = file("./myjenkins.pem")
+    host        = self.public_ip
+  }
+
   provisioner "remote-exec" {
-     inline = ["echo 'wait to start the instance' "]
+    inline = ["echo 'wait to start the instance' "]
   }
+
   tags = {
-     Name = "test-server"
-     }
-  provisioner "local-exec" {
-     command = "echo ${aws_instance.test-server.public_ip} > inventory"
-     }
-  provisioner "local-exec" {
-     command = "ansible-playbook /var/lib/jenkins/workspace/new/terraform-files/ansibleplaybook.yml"
-     }
+    Name = "test-server"
   }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.test-server.public_ip} > inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/new/terraform-files/ansibleplaybook.yml"
+  }
+}
